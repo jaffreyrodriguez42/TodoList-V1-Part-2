16cc169498474df5c712cdbac0606adeced3461f
Jaff <jaffreyrodriguez42@gmail.com>
2020-11-17 14:33:13 +0800

    Understanding Node Module Exports: How to pass function and data between files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const BodyParser = require('body-parser'); // this is a module for using post request in our app
+const date = require(__dirname + "/date.js");
 
 const app = express(); // we are creating a new instance of express
 
@@ -11,32 +12,15 @@ app.set('view engine', 'ejs'); // we are using the ejs module to our app
 
 app.use(BodyParser.urlencoded({extended:true})); // we use BodyParser obj to be able to use post request and pass data from the web page to the server
 
-app.use(express.static("public"));
+app.use(express.static("public")); // we use static method of express to create a public folder for static files like css, js and images
 
 app.listen(3000, function(){
 	console.log("The server is runnning on port 3000");
 });
 
 app.get('/', function(req, res){ // this is get request to the "/" route
-	let today;
-	let theDay;
-	let d = new Date(); // we created a new Date object
-	let day = d.getDay(); // we used the getDay method of Date object // output is 0-6
-	console.log(day);
-	if(day == 0 || day == 6){
-		today = "Weekend";
-	}else{
-		today = "Weekday";
-	}
-
-	let options = {        //second argument that will be used in the Date obj method toLocaleDateString
-		weekday: 'long',
-		day: 'numeric',
-		month: 'long',
-	}
-
-	theDay = d.toLocaleDateString("en-US", options); //it will output for ex "Wednesday. October 21"
-
+	let today = date.getDate();
+	let theDay = date.getDay();
 	res.render('list', {today : today, listTitle: theDay, newItems: items}); // we render the today and theDay variables to the ejs template list.ejs in views folder
 });
 
